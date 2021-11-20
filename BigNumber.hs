509e774ff0ccfcf338543b9785d15cc7c06dc494
Tiago Rodrigues <tiaguito.andre21764@gmail.com>
2021-11-20 17:48:14 +0000

    Add some helper functions

--- BigNumber.hs
+++ BigNumber.hs
@@ -7,20 +7,18 @@ import Data.Char
 -- Constructor
 
 -- Ideia a pensar: como fazer números negativos
--- Pensei assim: data BigNumber = EmptyList | Negative | BN Int BigNumber e usava Negative na primeira/ultima posiçao
+-- Pensei assim: data BigNumber = EmptyList | Negative BigNumber | BN Int BigNumber e usava Negative na primeira/ultima posiçao
 -- Ou entao usava-se na primeira /ultima posiçao um numero negativo
 -- Como quiseres 
 
-data BigNumber = EmptyList | BN Int BigNumber
+data BigNumber = EmptyList | Negative BigNumber | BN Int BigNumber deriving Show
 
-
-
--- Scanner -> Estado: Falta ver números negativos
+-- Scanner -> Estado: Feito
 -- Melhor usar na ordem inversa
 --      Quando fizermos as operações, começamos pelo ultimo digito devido ao carry
 
 -- Teste #1 : scanner "123" = BN 3 (BN 2 (BN 1 EmptyList)) quando usado com "deriving Show"
-
+-- Teste #2 : scanner "-123" = Negative (BN 3 (BN 2 (BN 1 EmptyList)))
 
 scanner :: String -> BigNumber
 scanner "" = EmptyList
@@ -28,15 +26,18 @@ scanner xs = scannerHelper xs EmptyList
 
 scannerHelper :: String -> BigNumber -> BigNumber
 scannerHelper "" (BN x bn) = BN x bn
-scannerHelper (c: xs) a = scannerHelper xs (BN (ord c - ord '0') a)
+scannerHelper (c: xs) a
+        | c == '-' = Negative (scannerHelper xs a)
+        | otherwise = scannerHelper xs (BN (ord c - ord '0') a)
 
-
--- Output -> Estado: Falta ver números negativos
+-- Output -> Estado: Feito
 
 -- Teste #1 : output (scanner "123") = "123"
+-- Teste #2 : output (scanner "-123") = "-123"
 
 output :: BigNumber -> String
 output EmptyList = ""
+output (Negative bn) = ('-' : outputHelper bn "")
 output (BN a bn) = outputHelper (BN a bn) ""
 
 outputHelper :: BigNumber -> String -> String
@@ -47,36 +48,42 @@ outputHelper (BN a bn) str = outputHelper bn (chr (a + ord '0') : str)
 -- SomaBN
 -- Teste #1:  output (somaBN (scanner "123") (scanner "123")) = "246"
 -- Teste #2:  output (somaBN (scanner "123") (scanner "12")) = "135"
--- Teste #3:  output (somaBN (scanner "123") (scanner "987")) = "0:::" --------- Ovbiamente errado
+-- Teste #3:  output (somaBN (scanner "123") (scanner "987")) = "1110"
 
 somaBN :: BigNumber -> BigNumber -> BigNumber
-somaBN EmptyList EmptyList = EmptyList                                          -- Alterar , acho que nao vai ser necessario
+somaBN EmptyList EmptyList = EmptyList
 somaBN EmptyList (BN x bn) = (BN x (somaBN EmptyList bn))
 somaBN (BN x bn) EmptyList = (BN x (somaBN bn EmptyList))
 somaBN (BN x bnx) (BN y bny) 
-  | ((x + y) < 10 && (x + y) > 0) = (BN (mod (x+y) 10) (somaBN bnx bny))                        -- Soma de dois números positivos com carry 
-  | (x + y) >= 10 = (BN (x+y) (somaBN (carry bnx (mod (x+y) 10)) bny))                   -- Soma de dois números positivos sem carry 
+  | ((x + y) < 10 && (x + y) > 0) = (BN (mod (x+y) 10) (somaBN bnx bny))                                      -- Soma de dois números positivos com carry 
+  | (x + y) >= 10 = (BN (mod (x+y) 10) (somaBN (somaBN bnx bny) (BN (div (x+y) 10) EmptyList)))               -- Soma de dois números positivos sem carry 
   | otherwise = EmptyList
-
-
 
 
 ----------------------
 -- Helper Functions --
 ----------------------
 
--- carry -> Estado: Falta para números negativos 
--- Podiamos ter usado a propria funçao SomaBN
--- Serve para tratar dos carries, decidi fazer em funçao separada mas se quiseres ver de outra maneira estou livre
--- Porque o int e não apenas +1? No caso de multiplicaçoes: 7*7 = 49 em que 4 de carry e 9 era o valor que ficava
+-- absBN -> Estado: Feito
+--      Gives the absolute number of BigNumber
 
--- Teste #1: output (carry (scanner "123") 3) = "126"
--- Teste #2: output (carry (scanner "123") 9) = "132"
+-- Teste #1: output(absBN(scanner("123"))) = "123"
+-- Teste #2: output(absBN(scanner("-123"))) = "123"
 
-carry :: BigNumber -> Int -> BigNumber
-carry EmptyList n = (BN n EmptyList)
-carry (BN x bn) n
-        | (x + n) >= 10 = BN (mod (x+n) 10) (carry bn (div (x+n) 10))
-        | otherwise = BN (x+n) bn
+absBN :: BigNumber -> BigNumber
+absBN EmptyList = EmptyList
+absBN (Negative bn) = bn
+absBN bn = bn
 
 
+-- isNegativeBN -> Estado: Feito
+--       Return True if it is Negative, false otherwise
+
+
+-- Teste #1: isNegativeBN(scanner("-123")) = True
+-- Teste #2: isNegativeBN(scanner("123")) = False
+-- Teste #3: isNegativeBN(EmptyList) = False
+
+isNegativeBN :: BigNumber -> Bool
+isNegativeBN (Negative bn) = True
+isNegativeBN _ = False
