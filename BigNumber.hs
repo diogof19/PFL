e294a00ad93787adb110bea2e104056df2d48fde
Skoubou9 <72798819+Skoubou9@users.noreply.github.com>
2021-11-23 11:42:22 +0000

    helper functions

--- BigNumber.hs
+++ BigNumber.hs
@@ -61,23 +61,23 @@ outputHelper (BN a bn) str = outputHelper bn (chr (a + ord '0') : str)
 
 somaBN :: BigNumber -> BigNumber -> BigNumber
 -- Caso Lista Acabou
-somaBN EmptyList EmptyList = EmptyList  
+somaBN EmptyList EmptyList = EmptyList
 -- Caso Lista Acabou e Numero Positivo
-somaBN EmptyList (BN x bn) = somaBN (BN x bn) EmptyList
+somaBN EmptyList (BN x bn) = removeZeros $ somaBN (BN x bn) EmptyList
 somaBN (BN x bn) EmptyList
   | x == 0 = EmptyList
   | otherwise = (BN x bn)
 -- Caso Dois Numeros Negativos
-somaBN (Negative bnx) (Negative bny) = Negative (somaBN bnx bny)
+somaBN (Negative bnx) (Negative bny) = removeZeros $ Negative (somaBN bnx bny)
 -- Caso Numero Positivo e Numero Negativo
 somaBN (Negative bnx) (BN y bny)
-  | isGreaterBN bnx (BN y bny) = Negative (subBN bnx (BN y bny))  
+  | isGreaterBN bnx (BN y bny) = removeZeros $ Negative (subBN bnx (BN y bny))
   | otherwise = subBN (BN y bny) bnx
-somaBN (BN x bnx) (Negative bny) = somaBN (Negative bny) (BN x bnx) 
+somaBN (BN x bnx) (Negative bny) = removeZeros $ somaBN (Negative bny) (BN x bnx)
 -- Caso Dois Numeros Positivos
 somaBN (BN x bnx) (BN y bny)
-  | ((x + y) < 10 && (x + y) > 0) = (BN (mod (x+y) 10) (somaBN bnx bny))
-  | (x + y) >= 10 = (BN (mod (x+y) 10) (somaBN (somaBN bnx bny) (BN (div (x+y) 10) EmptyList)))
+  | ((x + y) < 10 && (x + y) > 0) = removeZeros $ (BN (mod (x+y) 10) (somaBN bnx bny))
+  | (x + y) >= 10 = removeZeros $ (BN (mod (x+y) 10) (somaBN (somaBN bnx bny) (BN (div (x+y) 10) EmptyList)))
   | otherwise = EmptyList
 
 
@@ -94,21 +94,22 @@ subBN :: BigNumber -> BigNumber -> BigNumber
 -- Caso Listas Acabaram
 subBN EmptyList EmptyList = EmptyList
 -- Caso Lista Acabou e Numero Positivo
-subBN EmptyList (BN x bn) = Negative (subBN (BN x bn) EmptyList)
+subBN EmptyList (BN x bn) = removeZeros $ Negative (subBN (BN x bn) EmptyList)
 subBN (BN x bn) EmptyList
   | x == 0 = EmptyList
   | otherwise = (BN x bn)
 -- Caso Dois Numeros Negativos
-subBN (Negative bnx) (Negative bny) = somaBN (Negative bnx) bny
+subBN (Negative bnx) (Negative bny) = removeZeros $ somaBN (Negative bnx) bny
 -- Caso Numero Positivo e Numero Negativo
-subBN (Negative bnx) (BN y bny) = Negative (somaBN bnx (BN y bny))
-subBN (BN x bnx) (Negative bny) = somaBN (BN x bnx) bny
+subBN (Negative bnx) (BN y bny) = removeZeros $ Negative (somaBN bnx (BN y bny))
+subBN (BN x bnx) (Negative bny) = removeZeros $ somaBN (BN x bnx) bny
 -- Dois Numeros Positivos
 subBN (BN x bnx) (BN y bny)
-  | isEqualBN (BN x bnx) (BN y bny) = EmptyList
-  | isGreaterBN (BN x bnx) (BN y bny) && x < y = (BN (mod (x-y) 10) (subBN (subBN bnx (BN 1 EmptyList)) bny))
-  | isGreaterBN (BN x bnx) (BN y bny) && x >= y = (BN (x-y) (subBN bnx bny))
-  | otherwise = Negative (subBN (BN y bny) (BN x bnx)) 
+  | isEqualBN (BN x bnx) (BN y bny) = (BN 0 EmptyList)
+  | isGreaterBN (BN x bnx) (BN y bny) && x < y = removeZeros $ (BN (mod (x-y) 10) (subBN (subBN bnx (BN 1 EmptyList)) bny))
+  | isGreaterBN (BN x bnx) (BN y bny) && x >= y = removeZeros $ (BN (x-y) (subBN bnx bny))
+  | otherwise = removeZeros $ Negative (subBN (BN y bny) (BN x bnx))
+
 
 ----------------------
 -- Helper Functions --
@@ -187,3 +188,57 @@ isGreaterBN (BN x bnx) (BN y bny)
   | ((isEqualBN bnx EmptyList) && (isEqualBN bny EmptyList) || (isEqualBN bnx bny)) && (x > y) = True
   | ((isEqualBN bnx EmptyList) && (isEqualBN bny EmptyList) || (isEqualBN bnx bny)) && (x < y) = False
   | otherwise = isGreaterBN bnx bny
+
+-- lengthBN -> Estado: Feito
+--    Return the length of the BigNumber
+
+-- Teste #1: lengthBN (scanner "-123") = "3"
+-- Teste #2: lengthBN (scanner "1123") = "4"
+-- Teste #3: lengthBN (scanner "0") = "1"
+-- Teste #4: lengthBN (scanner "") = "0"
+
+lengthBN :: BigNumber -> Int
+lengthBN EmptyList = 0
+lengthBN (Negative bn) = lengthBNHelper bn 0
+lengthBN (BN x bn) = lengthBNHelper (BN x bn) 0
+
+
+lengthBNHelper :: BigNumber -> Int -> Int
+lengthBNHelper EmptyList n = n
+lengthBNHelper (BN v bn) n = lengthBNHelper bn (n+1)
+
+-- flipBN -> Estado: Feito
+
+-- Teste #1: flipBN(scanner "123") = BN 1 (BN 2 (BN 3 EmptyList))
+-- Teste #2: flipBN(scanner "-123") = Negative (BN 1 (BN 2 (BN 3 EmptyList)))
+
+flipBN :: BigNumber -> BigNumber
+flipBN EmptyList = EmptyList
+flipBN (Negative bn) = (Negative (flipBNHelper bn EmptyList))
+flipBN (BN n bn) = flipBNHelper (BN n bn) EmptyList
+
+flipBNHelper :: BigNumber -> BigNumber -> BigNumber
+flipBNHelper EmptyList bn = bn
+flipBNHelper (BN x bnx) EmptyList = flipBNHelper bnx (BN x EmptyList)
+flipBNHelper (BN x bnx) (BN y EmptyList) = flipBNHelper bnx (BN x (BN y EmptyList))
+flipBNHelper (BN x bnx) (BN y bny) = flipBNHelper bnx (BN x (BN y bny))
+
+-- removeZeros -> Estado:
+-- Return BigNumber without unnecessary zeros
+
+-- Teste #1: output (removeZeros (scanner "0")) = "0"
+-- Teste #2 output (removeZeros (scanner "0123")) = "123"
+-- Teste #3 output (removeZeros (scanner "000123")) = "123"
+
+removeZeros :: BigNumber -> BigNumber
+removeZeros EmptyList = EmptyList
+removeZeros (Negative bn) = Negative (removeZeros bn)
+removeZeros (BN x bnx)
+  | (lengthBN (BN x bnx) > 1) = flipBN (removeZerosAux (flipBN (BN x bnx)))
+  | otherwise = (BN x bnx)
+
+removeZerosAux :: BigNumber -> BigNumber
+removeZerosAux EmptyList = EmptyList
+removeZerosAux (BN x bnx)
+  | (x == 0 && lengthBN(BN x bnx) > 1) = removeZerosAux bnx
+  | otherwise = BN x bnx
